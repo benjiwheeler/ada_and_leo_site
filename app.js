8098429bfc93d29393cad1ae0b9793090d5fea6f
Ben Wheeler <wheeler.benjamin@gmail.com>
2016-05-20 08:39:21 -0400

    trying to fix teacher name problem

--- app.js
+++ app.js
@@ -177,6 +177,7 @@ app.controller('CoursesController', ['$scope', 'commonData', function($scope, co
   // gets all the teacher objects for the fullnames listed for each course
   $scope.getTeachers = function(teacherFullnameArr) {
     return _.map(teacherFullnameArr, function(teacherFullname) {
+      teacherFullname = "Ben Wheeler";
       var teacherRecord = _.find($scope.staff, {"fullname": teacherFullname});
       if ((teacherRecord !== undefined) && (teacherRecord !== null)) {
         console.log("found teacher with fullname: " + teacherFullname);
