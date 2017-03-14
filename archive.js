fed3a2724e796dd8ff844552e3fc8a4c03cf4e72
Ben Wheeler <wheeler.benjamin@gmail.com>
2017-03-13 23:22:16 -0400

    updated new courses

---
+++ archive.js
@@ -0,0 +1,361 @@
+// inactive info, for posterity
+
+          ,{"text": "Enroll: Summer program", "url": "http://goo.gl/forms/N98WN8BFiJVaPSBu1", "class": "log_enroll"}
+          ,{"text": "Summer program", "url": "index.html#summer"}
+      {"text": "Enroll in summer program", "url": "http://goo.gl/forms/N98WN8BFiJVaPSBu1", "class": "log_enroll"}
+      ,{"text": "Summer program: overview", "url": "index.html#summer"}
+
+  ,"courses_archive": [
+    {
+    "session": "Winter 2017"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "grades K-4"
+    ,"times": "Saturdays 10am-11:15am"
+    ,"dates": "Jan 21, 28; Feb 4, 11, 25; Mar 4, 11 (7 sessions)"
+    ,"teachers": ["Ben Wheeler"]
+    ,"location": "Trilok Center, 143 Waverly Ave (at Myrtle)"
+    ,"cost": "$280"
+    ,"status": "enroll"
+    ,"signupurl": enrollmentFormURL
+    ,"color": "#396"
+    ,"additional": [
+      "Class is capped at 6 students",
+      "Note: no class on Feb 18th"
+      ]
+    }
+    ,{
+    "session": "Winter 2017"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "grades 2-3"
+    ,"times": "Wednesdays 2pm-3:15pm"
+    ,"dates": "Jan 18, 25; Feb 1, 8, 15; Mar 1, 8, 15 (8 sessions)"
+    ,"teachers": ["Ben Wheeler"]
+    ,"location": "Trilok Center, 143 Waverly Ave (at Myrtle)"
+    ,"cost": "$320"
+    ,"status": "waitlist"
+    ,"signupurl": enrollmentFormURL
+    ,"color": "#b5f"
+    ,"additional": [
+      "Class is capped at 8 students",
+      "Note: no class on Feb 22nd",
+      "Optional: we will do pickup at Compass Charter School (300 Adelphi) at 1pm, transport students safely, and take care of students until the class starts at 2pm. Cost: $20 per pickup, or $120 if paid in advance for the full 8 classes (in which case the grand total is $440)."
+      ]
+    },
+    {
+    "session": "Late Fall 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Saturdays 10am-11:15am"
+    ,"dates": "Nov 5, 12, 19; Dec 3, 10, 17 (6 sessions)"
+    ,"teachers": ["Ben Wheeler"]
+    ,"location": "Trilok Center, 143 Waverly Ave (at Myrtle)"
+    ,"cost": "$240"
+    ,"status": "enroll"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#396"
+    ,"additional": [
+      ]
+    }
+    ,{
+    "session": "Late Fall 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Saturdays 12pm-1:15pm"
+    ,"dates": "Nov 5, 12, 19; Dec 3, 10, 17 (6 sessions)"
+    ,"teachers": ["Ben Wheeler"]
+    ,"location": "Trilok Center, 143 Waverly Ave (at Myrtle)"
+    ,"cost": "$240"
+    ,"status": "enroll"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#eb2"
+    ,"additional": [
+      ]
+    }
+    ,{
+    "session": "Late Fall 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Wednesdays 2pm-3:15pm"
+    ,"dates": "Nov 2, 9, 16, 30; Dec 7, 14, 21  (7 sessions)"
+    ,"teachers": ["Ben Wheeler"]
+    ,"location": "Trilok Center, 143 Waverly Ave (at Myrtle)"
+    ,"cost": "$280"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#b5f"
+    ,"additional": [
+      "Optional: we will do pickup at Compass Charter School (300 Adelphi) at 1pm, transport students safely, and take care of students until the class starts at 2pm. Cost: $20 per pickup, or $110 if paid in advance for the full 7 classes (in which case the grand total is $390)."
+      ]
+    }
+    ,{
+    "session": "Fall 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Saturdays 10am-11:15am"
+    ,"dates": "Sept 10, 17, 24; Oct 15, 22, 29 (6 sessions)"
+    ,"teachers": ["Ben Wheeler"]
+    ,"location": "Trilok Center, 143 Waverly Ave (at Myrtle)"
+    ,"cost": "$240"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#4a5"
+    ,"additional": [
+      "Note: Students and parents are encouraged to attend the Maker Faire on Sat Oct 1 or Sun Oct 2, where we will have a table and be showing student work! Children must be accompanied by an adult. <a href='http://makerfaire.com/new-york/'>Tickets and info</a>"
+      ]
+    }
+    ,{
+    "session": "Fall 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Saturdays 12pm-1:15pm"
+    ,"dates": "Sept 10, 17, 24; Oct 15, 22, 29 (6 sessions)"
+    ,"teachers": ["Ben Wheeler"]
+    ,"location": "Trilok Center, 143 Waverly Ave (at Myrtle)"
+    ,"cost": "$240"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#fa0"
+    ,"additional": [
+      "Note: Students and parents are encouraged to attend the Maker Faire on Sat Oct 1 or Sun Oct 2, where we will have a table and be showing student work! Children must be accompanied by an adult. <a href='http://makerfaire.com/new-york/'>Tickets and info</a>"
+      ]
+    }
+    ,{
+    "session": "Fall 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Wednesdays 2pm-3:15pm"
+    ,"dates": "Sept 14, 21, 28; Oct 5, 19, 26 (6 sessions)"
+    ,"teachers": ["Ben Wheeler", "Taylor Black"]
+    ,"location": "Trilok Center, 143 Waverly Ave (at Myrtle)"
+    ,"cost": "$240"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#97e"
+    ,"additional": [
+      "Optional: we will do pickup at Compass Charter School (300 Adelphi) at 1pm, transport students safely, and take care of students until the class starts at 2pm. Cost: $20 per pickup, or $90 if paid in advance for the full 6 classes (in which case the grand total is $330)."
+      ,"Note: Students and parents are encouraged to attend the Maker Faire on Sat Oct 1 or Sun Oct 2, where we will have a table and be showing student work! Children must be accompanied by an adult. <a href='http://makerfaire.com/new-york/'>Tickets and info</a>"
+      ]
+    }
+    ,{
+    "session": "Summer 2016"
+    ,"title": "Week 1: Creative programming"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Weekdays 9am-3:30pm"
+    ,"dates": "July 25-29 (5 days)"
+    ,"teachers": ["Ben Wheeler", "additional subject teachers on a daily basis"]
+    ,"location": "40 Brevoort Pl. (near Franklin C and Nostrand A/C stops)"
+    ,"cost": "$525"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/N98WN8BFiJVaPSBu1"
+    ,"color": "#fa0"
+    }
+    ,{
+    "session": "Summer 2016"
+    ,"title": "Week 2: Digital art"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Weekdays 9am-3:30pm"
+    ,"dates": "August 1-5 (5 days)"
+    ,"teachers": ["Ben Wheeler", "additional subject teachers on a daily basis"]
+    ,"location": "40 Brevoort Pl. (near Franklin C and Nostrand A/C stops)"
+    ,"cost": "$525"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/N98WN8BFiJVaPSBu1"
+    ,"color": "#fa0"
+    }
+    ,{
+    "session": "Summer 2016"
+    ,"title": "Week 3: Electric inventions"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Weekdays 9am-3:30pm"
+    ,"dates": "August 8-12 (5 days)"
+    ,"teachers": ["Ben Wheeler", "additional subject teachers on a daily basis"]
+    ,"location": "40 Brevoort Pl. (near Franklin C and Nostrand A/C stops)"
+    ,"cost": "$525"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/N98WN8BFiJVaPSBu1"
+    ,"color": "#fa0"
+    }
+    ,{
+    "session": "Spring 2016"
+    ,"title": "Intro to Game Design"
+    ,"eligible": "all children!"
+    ,"ages": "7-11 years"
+    ,"times": "Saturdays 10am-11:30pm (snack break provided)"
+    ,"dates": "May 14, 21; June 4, 11, 18 (5 sessions)"
+    ,"teachers": ["James Marion", "Pierre Depaz", "Chris Wallace"]
+    ,"location": "30 Macon St. (near the Nostrand A train stop)"
+    ,"cost": "$250"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#4a5"
+    ,"additional": "Note: one teacher's schedule is subject to cool applications they're waiting to hear back from! Class will have at least 2 teachers present at all times."
+    }
+    ,{
+    "session": "Spring 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-9 years"
+    ,"times": "Wednesdays 2pm-3:15pm"
+    ,"dates": "May 4, 11, 18, 25; June 1, 8, 15, 22 (8 sessions)"
+    ,"teachers": ["Taylor Black"]
+    ,"location": "30 Macon St. (near the Nostrand A train stop)"
+    ,"cost": "$240"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#77e"
+    ,"additional": "Optional: we will do pickup at Compass Charter School (300 Adelphi) at 1pm, transport students safely, and take care of students until the class starts at 2pm. Cost: $16 per pickup, or $100 if paid in advance for the full 8 classes (in which case the grand total is $340)."
+    }
+    ,{
+    "session": "Spring 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-9 years"
+    ,"times": "Saturdays 1pm-2:15pm"
+    ,"dates": "May 7, 14, 21; June 4, 11, 18, 25 (7 sessions)"
+    ,"teachers": ["Bryn Haffey", "Katya Vasilaky"]
+    ,"location": "30 Macon St. (near the Nostrand A train stop)"
+    ,"cost": "$220"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#97e"
+    ,"additional": "Note: Bryn and Katya will be trading off some classes. One or both of them will always be present at each class."
+    }
+    ,{
+    "session": "Spring 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-10 years"
+    ,"times": "Sundays 1:30pm-2:45pm"
+    ,"dates": "May 15, 22; June 5, 12, 19, 26 (6 sessions)"
+    ,"teachers": ["Hilary Fung"]
+    ,"location": "30 Macon St. (near the Nostrand A train stop)"
+    ,"cost": "$180"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#87e"
+    }
+    ,{
+    "session": "Spring 2016"
+    ,"title": "Intro to Animation"
+    ,"eligible": "all children!"
+    ,"ages": "7-11 years"
+    ,"times": "Sundays 11:30am-12:45pm"
+    ,"dates": "May 8, 15, 22; June 5, 12, 19 (6 sessions)"
+    ,"teachers": ["Dakota Pailes-Friedman"]
+    ,"location": "30 Macon St. (near the Nostrand A train stop)"
+    ,"cost": "$200"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#d35"
+    }
+    ,{
+    "session": "Spring 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "Students attending Brooklyn Coop School at Brevoort location"
+    ,"ages": "6-9 years"
+    ,"times": "Tuesdays 3:45pm-4:30pm"
+    ,"dates": "Apr 5, 12, 19; May 3, 10, 17, 24, 31; June 7, 14 (10 sessions)"
+    ,"teachers": ["Ben Wheeler"]
+    ,"location": "40 Brevoort Pl."
+    ,"cost": "$300"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#97e"
+    }
+    ,{
+    "session": "Winter 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-9 years"
+    ,"times": "Saturdays 2:30pm-3:45pm"
+    ,"dates": "Feb 27; Mar 5, 12, 19, 26; Apr 2, 9, 16, 23; May 7 (10 sessions)"
+    ,"teachers": ["Ben Wheeler", "Ayodamola Okunseinde"]
+    ,"location": "30 Macon St. (near the Nostrand A train stop)"
+    ,"cost": "$300"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#97e"
+    }
+    ,{
+    "session": "Winter 2016"
+    ,"title": "Intro to creative tech"
+    ,"eligible": "all children!"
+    ,"ages": "6-9 years"
+    ,"times": "Saturdays 9:30am-10:45am"
+    ,"dates": "Feb 6, 13, 27; Mar 5, 12, 19, 26; Apr 2, 9, 16 (10 sessions)"
+    ,"teachers": ["Ben Wheeler", "Audrey Fox"]
+    ,"location": "30 Macon St. (near the Nostrand A train stop)"
+    ,"cost": "$300"
+    ,"status": "closed"
+    ,"signupurl": "http://goo.gl/forms/z4IY2m1aLR"
+    ,"color": "#77e"
+    }
+  ]
+
+
+    ,{
+      "fullname": "Audrey Fox"
+      ,"nickname": "Audrey"
+      ,"bio": "Audrey is a MFA candidate in Design and Technology at Parsons in New York City. She is an experienced branding designer as well as a tech educator for children. Her current work focuses on using sensors, computer vision and Internet of Things to build <span style=\"background-color: yellow\">multi-sensory interactive environments</span>. See more at <a href=\"http://www.artificialforest.com/\">artificialforest.com</a>"
+      ,"nutshell": "children's tech educator, Parsons MFA candidate"
+    }
+    ,{
+      "fullname": "Ayodamola Okunseinde"
+      ,"nickname": "Ayo"
+      ,"bio": "Ayodamola (\"Ayo\" for short) is an artist and interactive designer, and is currently an adjunct faculty member at The New School, Parsons School of Design. His works range from painting and speculative design to <span style=\"background-color: yellow\">physically interactive works and explorations of Afrofuturism</span>. See more of his work at <a href=\"http://www.ayo.io/\">ayo.io</a>. "
+      ,"nutshell": "artist and interactive designer, Parsons adjunct faculty"
+    }
+    ,{
+      "fullname": "Dakota Pailes-Friedman"
+      ,"nickname": "Dakota"
+      ,"bio": "Dakota (AKA \"Dakota PF\") is a Brooklyn-born and -based artist. As half of <a href=\"https://vimeo.com/lowresfilm\">LOW-RES FILMS</a>, he directs, edits, writes and designs sound for <span style=\"background-color: yellow\">live-action and animation films</span>. Outside of film-making, Dakota also paints and creates digital design work, and teaches kids karate."
+      ,"nutshell": "veteran animator, director, and karate teacher"
+    }
+    ,{
+      "fullname": "Bryn Haffey"
+      ,"nickname": "Bryn"
+      ,"bio": "Bryn is a data engineer at an advertising technology company and holds a Master's in Computer Science.  She has prior experience teaching introductory programming courses to first year college students, but loves younger children, especially her own niece and nephew.  She lives with her husband and <span style=\"background-color: yellow\">rescue dog named Franklyn</span> in Clinton Hill and enjoys biking and Pixar movies. "
+      ,"nutshell": "data engineer, CS MS, dog lover"
+    }
+    ,{
+      "fullname": "Katya Vasilaky"
+      ,"nickname": "Katya"
+      ,"bio": "Katya is a postdoc in economics at Columbia University, an upcoming instructor at Girls Who Code in NYC 2016, akd a co-organizer of Pyladies NYC. She also <span style=\"background-color: yellow\">volunteers as a dance teacher</span> in Harlem schools and has danced ballet professinally. See more on <a href=\"https://katyavasilaky.com/\">her site.</a>"
+      ,"nutshell": "Econ PhD and dance teacher"
+    }
+    ,{
+      "fullname": "Chris Wallace"
+      ,"nickname": "Chris"
+      ,"bio": "Chris is an experienced game designer, web developer, and project manager that grew up in New York. He's trained and taught kung fu to people of all ages for 13 years at the USA Shaolin Temple. He also enjoys traveling and walking up mountains in the middle of the night."
+      ,"nutshell": "game designer and kung-fu teacher"
+    }
+    ,{
+      "fullname": "Hilary Fung"
+      ,"nickname": "Hilary"
+      ,"bio": "Hilary works at the Huffington Post, where she reports, designs and develops data stories and multimedia features. She has also worked with kids through Sierra Club NYC ICO, Camp/Interactive, and more."
+      ,"nutshell": "data journalist and designer at Huffington Post"
+    }
+    ,{
+      "fullname": "James Marion"
+      ,"nickname": "James"
+      ,"bio": "James has worked on many bestselling and award-winning games, from smash hit <i>Two Dots</i> to his acclaimed \"mobile musical\" <i>Peter Panic</i>, produced with Broadway and game design legends. He holds an MFA in game design from Tisch, won “Best Game” award at the 2015 Global Game Jam, and been published on several popular game-related websites."
+      ,"nutshell": "award-winning game designer, NYU MFA"
+    }
+    ,{
+      "fullname": "Pierre Depaz"
+      ,"nickname": "Pierre"
+      ,"bio": "Pierre is a software developer who grew up in Europe and settled in New York to complete a Masters in Game Design. He is currently working as a freelancer for multiple companies in the tech and arts world and is an adjunct faculty member at NYU. He also volunteers for the afterschool program ScriptEd, and is eager to introduce kids to Processing/p5, Arduino and creating games!"
+      ,"nutshell": "adjunct faculty member, NYU Game Center"
+    }
+
